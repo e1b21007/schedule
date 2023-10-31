1881935216747b349f7fc2f5ffb93bc3e3fb715d
e1b21007 <e1b21007@oit.ac.jp>
2023-10-31 15:23:49 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/team7/schedule/ScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team7.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
