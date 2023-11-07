27e97148316b9dbfe8c1f0cf32c9b4d633dfb5fb
B21102 <e1b21102@oit.ac.jp>
2023-11-07 14:30:21 +0900

    controller変更

--- src/main/java/oit/is/team7/schedule/controller/schedule_controller.java
+++ src/main/java/oit/is/team7/schedule/controller/schedule_controller.java
@@ -5,13 +5,19 @@ import javax.annotation.processing.Generated;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class schedule_controller {
 
   @GetMapping("/calendar")
-  public String calendar() {
+  public String calendar(@RequestParam Integer id) {
     return "calendar.html";
+  }
+
+  @GetMapping("/post")
+  public String post(@RequestParam Integer id) {
+    return "post.html";
   }
 
   @GetMapping("/home")
