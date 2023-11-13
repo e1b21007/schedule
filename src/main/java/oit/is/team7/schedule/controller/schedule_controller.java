9614106224280ad858cc3da2bfdbae31004c9108
b21112 <e1b21112@oit.ac.jp>
2023-11-13 22:02:50 +0900

    home.html,content.htmlのthyeleaf処理の実装

--- src/main/java/oit/is/team7/schedule/controller/schedule_controller.java
+++ src/main/java/oit/is/team7/schedule/controller/schedule_controller.java
@@ -2,7 +2,11 @@ package oit.is.team7.schedule.controller;
 
 import oit.is.team7.schedule.model.sgroup;
 import oit.is.team7.schedule.model.sgroupMapper;
+import oit.is.team7.schedule.model.groupSchedule;
 import oit.is.team7.schedule.model.groupScheduleMapper;
+
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -31,19 +35,33 @@ public class schedule_controller {
     return "post.html";
   }
 
-  @PostMapping ("/calendar")
+  @PostMapping("/calendar")
   public String calendar(@RequestParam Integer id,
-                         @RequestParam String title, @RequestParam String date,
-                         @RequestParam String start, @RequestParam String end,
-                         @RequestParam String content,
-                         ModelMap model) {
+      @RequestParam String title, @RequestParam String date,
+      @RequestParam String start, @RequestParam String end,
+      @RequestParam String content,
+      ModelMap model) {
     groupschedulemapper.insertGroupSchedule(date, start, end, id, title, content);
 
     return "calendar.html";
   }
 
   @GetMapping("/home")
-  public String home() {
+  public String home(ModelMap model) {
+    ArrayList<sgroup> groups = sgroupmapper.selectAllSgroup();
+
+    model.addAttribute("groups", groups);
+
     return "home.html";
   }
+
+  @GetMapping("/detail")
+  public String content(ModelMap model) {
+    groupSchedule groupSchedule = groupschedulemapper.getgroupScheduleByScheduleid(1);
+
+    model.addAttribute("groupSchedule", groupSchedule);
+
+    return "content.html";
+  }
+
 }
