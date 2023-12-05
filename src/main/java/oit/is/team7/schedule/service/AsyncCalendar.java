87aea4b4c8a14deb45943d94ab11d4a8f3c05904
B21102 <e1b21102@oit.ac.jp>
2023-12-05 16:37:54 +0900

    Date型4thmob

--- src/main/java/oit/is/team7/schedule/service/AsyncCalendar.java
+++ src/main/java/oit/is/team7/schedule/service/AsyncCalendar.java
@@ -14,7 +14,9 @@ import org.slf4j.LoggerFactory;
 import java.time.ZonedDateTime;
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.TimeZone;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -82,20 +84,27 @@ public class AsyncCalendar {
     LocalDate localDate;
     Date nowDate;
     String test = "test";
+    Date date = new Date();
     try {
       while (true) {
-        now = ZonedDateTime.now();
-        localDate = now.toLocalDate();
-        nowDate = Date.valueOf(localDate);
-        emitter.send(nowDate);
-        ArrayList<GroupSchedule> schedule_list = groupschedulemapper.selectgroupScheduleByGroupid(id);
-        for (GroupSchedule schedule : schedule_list) {
-          if (schedule.getHizuke().equals(now.toString())) {
-
-          }
-          if(nowDate.compareTo(schedule.getHizuke())){}
-        }
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        c.setTimeZone(TimeZone.getTimeZone("GMT-9"));
+        emitter.send(c);
         TimeUnit.SECONDS.sleep(1);
+        // now = ZonedDateTime.now();
+        // localDate = now.toLocalDate();
+        // nowDate = Date.valueOf(localDate);
+        // emitter.send(nowDate);
+        // ArrayList<GroupSchedule> schedule_list =
+        // groupschedulemapper.selectgroupScheduleByGroupid(id);
+        // for (GroupSchedule schedule : schedule_list) {
+        // if (schedule.getHizuke().equals(now.toString())) {
+
+        // }
+        // if(nowDate.compareTo(schedule.getHizuke())){}
+        // }
+        // TimeUnit.SECONDS.sleep(1);
       }
     } catch (Exception e) {
       // TODO: handle exception
