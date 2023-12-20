bc37be736d34d67e5fd64535e1b0972a2d55112d
naoya0117 <81454816+naoya0117@users.noreply.github.com>
2023-12-20 22:41:56 +0900

    グループメンバ編集機能を追加

--- src/main/java/oit/is/team7/schedule/model/GroupsMapper.java
+++ src/main/java/oit/is/team7/schedule/model/GroupsMapper.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface GroupsMapper {
